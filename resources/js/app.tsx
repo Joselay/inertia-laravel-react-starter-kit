d63bdae52ec29fdd00e43d70d77a51cad81d3a6c
Smae Tongmenglay <smaetongmenglay@MENGLAY.local>
2024-11-27 21:41:21 +0700

    implemented starter-kit for postgresql

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,11 +4,24 @@ import "../css/app.css";
 import { createInertiaApp } from "@inertiajs/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Layout from "./Layouts/Layout";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.tsx", { eager: true });
-        return pages[`./Pages/${name}.tsx`];
+        const pages: Record<
+            string,
+            { default: React.FC & { layout?: React.FC } }
+        > = import.meta.glob("./Pages/**/*.tsx", {
+            eager: true,
+        });
+
+        let page = pages[`./Pages/${name}.tsx`];
+        page.default.layout =
+            page.default.layout ||
+            (((page: React.ReactNode) => (
+                <Layout children={page} />
+            )) as React.FC);
+        return page;
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
